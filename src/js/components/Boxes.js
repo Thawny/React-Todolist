2b146086f0d3c0d5ab5f7aab32bbe68858c4b7a3
Tawny <tony.malto.simon@gmail.com>
2017-09-25 17:15:02 +0200

    OK : drag 'n drop

--- src/js/components/Boxes.js
+++ src/js/components/Boxes.js
@@ -5,68 +5,115 @@ export default class Boxes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tasks: this.props.todos,
-            doing: [],
-            done: []
+            todos: this.props.todos,
+            doings: [],
+            dones: []
         }
         this.handleClick = this.handleClick.bind(this);
+        this.originBox = "";
     }
 
-    renderTasks() {
-        var Tasks = this.state.tasks.map((task, i) =>
+    renderTasks(targetBox) {
+        var Tasks = this.state[targetBox].map((task, i) =>
             <Task key={i.toString()} taskContent={task} updateTasks={this.updateTasks.bind(this)}/>
         );
         return Tasks;
     }
 
     // "taskName", "UPDATE || DELETE || ADD" --> setState()
-    updateTasks(action, taskName, newTask) {
+    updateTasks(targetBox ,action, taskName, newTask) {
         if (action == "DELETE") {
-            const newState = this.state.tasks.filter(task => task !== taskName)
+            const newState = this.state[targetBox].filter(task => task !== taskName)
             this.setState((prevState, props) => ({
-            tasks: newState
+            [targetBox]: newState
             }));
         } else if (action == "UPDATE") {
-            const newState = this.state.tasks.map(task => {
+            const newState = this.state[targetBox].map(task => {
                 return task == taskName ? newTask : task
             })
             this.setState((prevState, props) => ({
-            tasks: newState
+            [targetBox]: newState
             }));
         } else if (action == "ADD") {
-            this.setState((prevState, props) => ({
-                tasks: prevState.tasks.concat([""])
-            }));
-            console.log(this.state.tasks)
+            // si newtask la prendre en compte sinon tâche vide
+            if (newTask) {
+                this.setState((prevState, props) => ({
+                    [targetBox]: prevState[targetBox].concat([newTask])
+                }));
+            } else {
+                this.setState((prevState, props) => ({
+                    [targetBox]: prevState[targetBox].concat([""])
+                }));
+            }
         }
     }
 
     handleClick() {
-        console.log('click add')
-        this.updateTasks("ADD")
+        this.updateTasks("todos", "ADD")
+    }
+
+    handleDrop(e) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
+        const newBox = e.currentTarget.dataset['type'];
+        const originBox = this.originBox;
+        const movedTask = e.dataTransfer.getData("text");
+        if (e.currentTarget.classList.contains('dragged-over')) {
+            e.currentTarget.classList.remove('dragged-over');
+        }
+        // TODO: supprimer tâche de l'ancien conteneur et le mette dans le nouveau
+        // supprimer la tâche de l'ancien :
+        this.updateTasks(originBox, "DELETE", movedTask);
+        // ajouter la tâche dans le conteneur du drop :
+        this.updateTasks(newBox, "ADD", null, movedTask);
+
+        e.dataTransfer.clearData();
+        this.originBox = "";
+    }
+
+    handleDragOver(e) {
+        e.preventDefault()
+        if (!e.currentTarget.classList.contains('dragged-over')) {
+            e.currentTarget.classList.add('dragged-over')
+        }
+
+    }
+
+    handleDragStart(e) {
+        this.originBox = e.currentTarget.dataset['type'];
+        e.dataTransfer.setData('text', e.target.querySelector('.task-content').innerHTML);
+    }
+
+    handleDropLeave(e) {
+        e.stopPropagation()
+        if (e.currentTarget.classList.contains('dragged-over')) {
+            e.currentTarget.classList.remove('dragged-over')
+        }
     }
 
     render () {
         return (
-            <div className="box-container box-container-shrink" >
-                <div className="to-do box">
+            <div className="box-container box-container-shrink">
+                <div data-type="todos" className="to-do box" onDrop={this.handleDrop.bind(this)} onDragStart={this.handleDragStart.bind(this)} onDragLeave={this.handleDropLeave} onDragOver={this.handleDragOver}>
                     <h3>
                         <span>À FAIRE</span>
                     </h3>
-                    {this.renderTasks()}
+                    {this.renderTasks('todos')}
                     <div className="task add-task-container">
                         <span className="add-task" onClick={this.handleClick.bind(this)}>+</span>
                     </div>
                 </div>
-                <div className="doing box">
+                <div data-type="doings" className="doing box" onDragStart={this.handleDragStart.bind(this)} onDrop={this.handleDrop.bind(this)} onDragLeave={this.handleDropLeave} onDragOver={this.handleDragOver}>
                     <h3>
                         <span>EN COURS</span>
                     </h3>
+                    {this.renderTasks('doings')}
                 </div>
-                <div className="done box">
+                <div data-type="dones" className="done box" onDragStart={this.handleDragStart.bind(this)} onDragOver={this.handleDragOver} onDrop={this.handleDrop.bind(this)} onDragLeave={this.handleDropLeave}>
                     <h3>
                         <span>FAIT</span>
                     </h3>
+                    {this.renderTasks('dones')}
                 </div>
             </div>
 
--- src/js/components/Task.js
+++ src/js/components/Task.js
@@ -20,7 +20,8 @@ export default class Task extends React.Component {
     componentDidMount() {
         this.toggleWidgetHook = document.getElementById(this.id).querySelector('.toggleWidgetHook');
         this.taskContent = document.getElementById(this.id).querySelector('.task-content');
-        this.actionWidget = document.getElementById(this.id).querySelectorAll('.action-widget')
+        this.actionWidget = document.getElementById(this.id).querySelectorAll('.action-widget');
+        this.draggableDiv = document.getElementById(this.id);
     }
 
     handleMouseEnter(e) {
@@ -32,29 +33,55 @@ export default class Task extends React.Component {
     }
 
     handleEditClick(e) {
-        console.log(this.taskContent.setAttribute("contenteditable",true))
-        this.taskContent.addEventListener('blur', e => this.taskContent.setAttribute("contenteditable",false))
+        // turn off draggable
+        this.draggableDiv.setAttribute('draggable', false);
+
+        this.taskContent.setAttribute("contenteditable",true);
+        this.taskContent.focus();
+
     }
 
     handleDeleteClick(e) {
         console.log(this.props.taskContent)
-        this.props.updateTasks("DELETE", this.props.taskContent)
+        this.props.updateTasks("todos" ,"DELETE", this.props.taskContent)
+    }
+
+    handleDragStart(e) {
+        console.log('drag started')
+    }
+
+    handleBlur(e) {
+        e.target.setAttribute("contenteditable",false);
+        this.draggableDiv.setAttribute('draggable', true);
     }
 
     render () {
         const offSet = ((!!window.chrome && !!window.chrome.webstore) || (typeof InstallTrigger !== 'undefined')) ? "-10px" : "0px";
         return (
-            <div id={this.id} className="task">
+            <div id={this.id} className="task" draggable onDragStart={this.handleDragStart}>
                 <span className="task-span"
                     onMouseEnter={this.handleMouseEnter.bind(this)}
                     onMouseLeave={this.handleMouseLeave.bind(this)} >
 
-                    <span className="task-content" contentEditable="false" style={{transform:'translateY('+ offSet +')'}}>
+                    <span className="task-content" contentEditable="false" onBlur={this.handleBlur.bind(this)} style={{transform:'translateY('+ offSet +')'}}>
                         {this.props.taskContent}
                     </span>
 
                     <span className="toggleWidgetHook">
-                        <span className="action-widget" onClick={this.handleEditClick.bind(this)}>E</span>
+                        <span className="action-widget" onClick={this.handleEditClick.bind(this)}>
+
+                            <svg x="0px" y="0px"
+                            	 width="528.899px" height="528.899px" viewBox="0 0 528.899 528.899"
+                            	>
+                            <g>
+                            	<path d="M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981
+                            		c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611
+                            		C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069
+                            		L27.473,390.597L0.3,512.69z"/>
+                            </g>
+                            </svg>
+
+                        </span>
                         <span className="action-widget" onClick={this.handleDeleteClick.bind(this)}>X</span>
                     </span>
 
